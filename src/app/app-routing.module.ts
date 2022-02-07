1fc815a2fc0c82508f0c09428f542b2bdb8a817e
JeffryGonzalez <jeff@hypertheory.com>
2022-02-07 15:00:58 -0500

    got our feature module for counter going

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
@@ -7,7 +7,11 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
   },
-  // other routes
+  {
+    path: 'counter',
+    loadChildren: () =>
+      import('./features/counter/counter.module').then((m) => m.CounterModule),
+  },
   {
     path: '**',
     redirectTo: 'dashboard',
@@ -15,7 +19,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
